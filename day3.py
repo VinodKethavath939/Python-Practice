7dd4dd2bcc27de1dafb286e3ca2f86dd1d54eda5
Vinod7710 <vinodkethavath003@gmail.com>
2025-08-06 22:01:06 +0530

    Initial commit

---
+++ day3.py
@@ -0,0 +1,8 @@
+import math
+class Circle:
+    def __init__(self,r):
+        self.radius =r
+    def area(self):
+        return math.pi*self.radius**2
+c1=Circle(5)
+print("Area of the circle:",c1.area())
---
+++ day4.py
@@ -0,0 +1,25 @@
+# a=2
+# print(float(a))
+
+# import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+
+# print(arr)
+
+# print(type(arr))
+
+# import numpy as np
+# arr = np.array(42)
+# print(arr)
+
+# import numpy as np
+# arr = np.array(100)
+# print(arr.shape)
+
+# scalar = arr.item()
+# print(scalar)
+
+import numpy as np
+a = np.array([[1,2,3],[4,5,6]])
+print(a)
